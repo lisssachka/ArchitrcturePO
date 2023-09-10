13e90dfccaaacc620244451060dd4605e599d435
lisssachka <114672738+lisssachka@users.noreply.github.com>
2023-09-10 13:14:08 +0400

    Added Homework12

---
+++ homework12/java/example/MathOperations.java
@@ -0,0 +1,31 @@
+package example;
+
+public class MathOperations {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return (double) a / b;
+    }
+    public int findMax(int a, int b) {
+        return a > b ? a : b;
+    }
+    public int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
